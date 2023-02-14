4ae6fbef9821e133f7392ab4f5fe5589c712099b
Stingo <637nad@gmail.com>
2023-02-14 21:52:50 +0100

    Deleting Controller

---
+++ api/controllers/crud/delete.js
@@ -0,0 +1,30 @@
+const innovant = require("../../../db/models/lgl/innovant");
+const Startup = require("../../../db/models/lgl/startup");
+
+const deleteDoc = async (req, res) => {
+  let dbPointer;
+  if (req.user.role != "admin") return res.send({ message: "Unauthorized" });
+  const { type, _id } = req.body;
+  if (!_id)
+    return res.send({ message: "The _id is required for this operation" });
+  switch (type) {
+    case "PI":
+      dbPointer = innovant;
+      break;
+    case "ST":
+      dbPointer = Startup;
+      break;
+
+    default:
+      return res.send({ error: "Invalid Type" });
+  }
+  try {
+    const doc = await dbPointer.findByIdAndDelete(_id);
+    if (!doc) return res.send({ message: "Document doesnt exist" });
+    return res.send({ success: true });
+  } catch (error) {
+    res.send({ error });
+  }
+};
+
+module.exports = deleteDoc;
