fea2962b7da0a3a1010b43ba9e1140bf59c67673
Stingo <637nad@gmail.com>
2023-02-14 21:52:10 +0100

    Adding Routes For Updating And Deleting

--- api/routes/router.js
+++ api/routes/router.js
@@ -3,9 +3,13 @@ const Auth = require("../middlewares/auth");
 const Signup = require("../controllers/user/signup");
 const Signin = require("../controllers/user/signin");
 const Create = require("../controllers/crud/create")
+const Update = require("../controllers/crud/update")
+const Delete = require("../controllers/crud/delete")
 
 router.route("/signup").post(Signup);
 router.route("/signin").post(Signin);
 router.route("/create").post(Auth,Create)
+router.route("/update").post(Auth,Update)
+router.route("/delete").post(Auth,Delete)
 
 module.exports = router;
