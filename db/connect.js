de0b60a65d7902d975e14b422c62526ffa2d38fa
Stingo <637nad@gmail.com>
2023-02-16 17:24:03 +0100

    random

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ mongoose.set("strictQuery", true);
 require("dotenv").config();
 
 const connectDB = () =>
-  mongoose.connect("mongodb://localhost:27017/admin", {
+  mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
