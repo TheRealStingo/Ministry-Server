d6949dd23681f543ea3cc3a5ff9e9c21f4808e9f
Stingo <637nad@gmail.com>
2023-02-16 17:35:59 +0100

    ADDED MONGO ENV

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ mongoose.set("strictQuery", true);
 require("dotenv").config();
 
 const connectDB = () =>
-  mongoose.connect("mongodb://localhost:27017/admin", {
+  mongoose.connect(process.env.MONGO_URIV2, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
