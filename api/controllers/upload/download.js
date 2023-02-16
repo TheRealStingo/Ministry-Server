a8222d48cc18708dff24a802613b051211731b8f
Stingo <637nad@gmail.com>
2023-02-16 19:41:36 +0100

    added error message

--- api/controllers/upload/download.js
+++ api/controllers/upload/download.js
@@ -1,6 +1,8 @@
 const Download = async (req, res) => {
   const { fileUid } = req.params;
-  return res.download(`uploads/${fileUid}`);
+  return res.download(`uploads/${fileUid}`, (err) => {
+    res.send({ message: "File Not found" });
+  });
 };
 
 module.exports = Download;
