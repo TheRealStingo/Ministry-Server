92f14714c3f65f2bd85a9c5e1c7a21113e8d6886
Stingo <637nad@gmail.com>
2023-02-16 18:38:06 +0100

    env

--- db/connect.js
+++ db/connect.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", true);
 
 
 const connectDB = () =>
-  mongoose.connect(process.env.MONGO_URIV2, {
+  mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
