b1795ca3bd7830bbec82aad2be1dbcc4929e119c
Stingo <637nad@gmail.com>
2023-02-16 17:46:18 +0100

    Trying to fix issue

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
-require("dotenv").config();
+
 
 const connectDB = () =>
   mongoose.connect(process.env.MONGO_URIV2, {
