c2334c99ddde4918001642d806b85f4eef89006c
Stingo <637nad@gmail.com>
2023-02-17 14:19:35 +0100

    Fixed UNDEFINED Bearer bug

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -3,7 +3,9 @@ const jwt = require("jsonwebtoken");
 
 const authorization = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+
+    const token = req.headers.authorization?.split(" ")[1] || null
+    if(!token) return res.send({error:"Missing Bearer"})
     const user = jwt.verify(token, process.env.JWT_SECRET);
     req.user = user;
     return next();
